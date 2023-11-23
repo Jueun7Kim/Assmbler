bee97d01a9c5e0a987447d271f0a6e147bdd708c
Jueun7Kim <wndmsrla1234@gmail.com>
2023-11-23 20:27:46 +0900

    edit main

--- Assmbl/src/Assm/Main.java
+++ Assmbl/src/Assm/Main.java
@@ -7,10 +7,10 @@ import java.util.HashMap;
 
 public class Main {
     public static Map<String, String> registers = new HashMap<>();
+    public static ArrayList<String> stack = new ArrayList<>();
     public static int ZF = 0;
     public static int CF = 0;
     public static int i = 0;
-    public static ArrayList<String> stack = new ArrayList<>();
     
 
     	
